9a46b6388272f617d4abeee2f960c2dc06e179a4
Your Name <your.email@example.com>
2026-06-26 02:01:40 +0530

    feat: Set document title dynamically on Register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import RegistrationForm from '../components/RegistrationForm';
 
 const Register = () => {
+  useEffect(() => {
+    document.title = 'Create Account | SuperApp';
+  }, []);
+
   return <RegistrationForm />;
 };
 
